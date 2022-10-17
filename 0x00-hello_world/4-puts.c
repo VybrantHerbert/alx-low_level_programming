9834ed7078b430908971e5371d3c2598a27367cd
VybrantHerbert <ohefedjarho@yahoo.com>
2022-10-17 13:08:13 -0700

    This course isn't easy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	/**
+	 * puts - Ouutputs a string literal
+	 *
+	 * Return: String literal
+	 */
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
 
