97d2af17c3cc6fa913695746ab6f5e0b42956926
VybrantHerbert <ohefedjarho@yahoo.com>
2022-10-17 13:24:41 -0700

    Omo programming no easy oh

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/**
+	 * printf - Outputs arguments passed to it
+	 *
+	 */
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
