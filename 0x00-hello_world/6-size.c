84c31ce52920baab7285780f50a6d24600424ad8
VybrantHerbert <ohefedjarho@yahoo.com>
2022-10-17 19:40:37 -0700

    It has worked

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,15 +10,10 @@ int main(void)
 	 * printf - outputs some strings to standard output
 	 * sizeof_function - returns the sizes of a datatype
 	 */
-	
-	
-	printf("Size of a char: %lu byte(s)\n", 
-sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", 	sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", 
-sizeof(long));
-	printf("Size of a long long int: %lu byte(s)\n",
-sizeof(long long));
-	printf("Size of a float: %lu byte(s)\n", 	sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n",	sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
